c01ef40fa5c3ed02c1917c995d2d3edf26a6a5fb
samarthsharma07 <75937008+samarthsharma07@users.noreply.github.com>
2021-02-17 19:21:38 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,43 @@
-//WAP to find the sum of two fractions.
+#include<stdio.h>
+struct fractions
+{
+	int num, den;
+};
+typedef struct fractions fractions;
+struct fractions input(struct fractions a)
+{
+	scanf("%d%d", &a.num, &a.den);
+	return a;
+}
+int gcd(int a, int b)
+{
+	int gcd;
+	for(int i=1; i<=a && i<=b; i++)
+	if(a%i==0 && b%i==0)
+	gcd = i;
+	return gcd;
+}
+struct fractions addition(fractions a, fractions b)
+{
+    struct fractions c;
+	int num = ((a.num*b.den)+(b.num*a.den));
+	int den = (a.den*b.den);
+	int com = gcd(num, den);
+	c.num = num/com;
+	c.den = den/com;
+	return c;
+}
+void output(struct fractions c)
+{
+	printf("The sum is %d/%d\n", c.num, c.den);
+}
+int main()
+{
+	struct fractions a,b;
+	printf("Enter first fraction:");
+	a=input(a);
+	printf("Enter second fraction:");
+	b=input(b);
+	output(addition(a,b));
+	return 0;
+}
